12d8d65e0c5ffdbf61477569af1a6ef1e8d293a0
gautamrajesh007 <gautamrdev7@gmail.com>
2023-12-15 10:27:09 +0530

    Added @Enitiy annotation

--- src/main/java/org/example/teamup/Model/Users/User.java
+++ src/main/java/org/example/teamup/Model/Users/User.java
@@ -1,4 +1,14 @@
 package org.example.teamup.Model.Users;
 
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
 public class User {
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+
 }
